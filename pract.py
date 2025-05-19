4d89c463e31f3005c9f0c385c19d62e600f62974
RaymondMukonda <Raymondkogen@gmail.com>
2025-05-19 10:34:16 +0200

    pp.py

--- pract.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-today = datetime.now()
-print("today is: "+ str(today))
-
-one_day = timedelta(days=1)
-yesterday = today - one_day
-print("yesterday was: " + str(yesterday))
