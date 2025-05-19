c60f0a0f5e3ea1ffda58f88d8b59f5880833e87c
RaymondMukonda <Raymondkogen@gmail.com>
2025-05-19 17:35:43 +0200

    buttons.html

--- helpers.py
+++ helpers.py
@@ -1,4 +1,14 @@
+from colorama import init, Fore
+
 def display(message, is_warning=False):
     if is_warning:
-        print("This is a warning")
-    print(message)
+        print(Fore.RED + message)
+    else:
+         print(Fore.BLUE + message)
+
+def mode(massage, is_warning=False):
+    if is_warning:
+        print(Fore.RED + massage)
+    else:
+        print(Fore.BLUE + massage)
+
--- pp.py
+++ pp.py
@@ -1,3 +1,8 @@
 import helpers
 
 helpers.display("sample massage", True)
+
+print()
+
+from helpers import display
+display("Sample massage")
