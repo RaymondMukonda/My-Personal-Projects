8df9f44c0a661cb581a2726841b017e0a6a091ad
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-07 15:36:54 +0200

    apps.py

--- apps.py
+++ apps.py
@@ -1,33 +1,7 @@
-# Food = True
-# answer = input("is food edible true/false? ")
-# if answer == "yes":
-#     print("you are right food is edible")
-# else:
-#     print("the answer is true not false")
+def hello():
+    print("Greeting's to you stranger! 👽")
 
-# print(type(Food))
+human_speak = input("are you a human? ")
 
-# num1 = "23"
-# num2 = 23
-
-# print(int(num1) + num2)
-# print(type(num1 and num2))
-
-# users = ["dave", "john", "rammy"]
-
-# users.append("marry")
-# users.insert(1, "jerry")
-# users.reverse()
-# mycopy = users.copy()
-# mycopy.insert(1, "susan")
-
-# print(users)
-# print(mycopy)
-
-value = 1
-
-while value < 11:
-    print(value)
-    if value == 5:
-        break
-    value += 1
+if human_speak.lower() == "yes":
+    hello()
