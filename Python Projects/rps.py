a6dff60b8e18da3352bbcec9cfbc50a2f4b9c063
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-07 08:33:59 +0200

    rps.py

---
+++ Python Projects/rps.py
@@ -0,0 +1,39 @@
+import sys
+import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+while True:
+    print("")
+    playerchoice = input("Enter...\n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n\n")
+    player = int(playerchoice)
+
+    if player < 1 or player > 3:
+        sys.exit("You must enter 1, 2, 3.")
+
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
+
+    print("")
+    print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+    print("Pyton chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+    print("")
+    if player == 1 and computer == 3:
+        print("You have won!😲🔥 ")
+    elif player == 2 and computer == 1:
+        print("You have won!😊👌 ")
+    elif player == 3 and computer == 2:
+        print("You have won!😆✨ ")
+    elif player == computer:
+        print("It's a tie!😒")
+    else:
+        print("Python has won🐍🐍 ")
+
+    play_again = input("\nWould you like to play again? (y/n): ").lower()
+    if play_again != "y":
+        print("Thanks for playing!")
+        break
--- apps.py
+++ apps.py
@@ -1,5 +1,17 @@
-pizza = "dave"
-last = "mukonda"
+# Food = True
+# answer = input("is food edible true/false? ")
+# if answer == "yes":
+#     print("you are right food is edible")
+# else:
+#     print("the answer is true not false")
 
-print(type(pizza))
-print(type(pizza) == str)
+# print(type(Food))
+
+# num1 = "23"
+# num2 = 23
+
+# print(int(num1) + num2)
+# print(type(num1 and num2))
+
+value = input("please enter a value: ")
+print(value)
