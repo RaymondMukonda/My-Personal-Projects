43c996a36636c14c794f39243be7c9956edef29f
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-10 09:08:04 +0200

    apps.py

--- apps.py
+++ apps.py
@@ -3,3 +3,28 @@ def sum(num1,num2):
 
 total = sum(2, 3)
 print(total)
+
+def multiple_items(*args):
+    print(args)
+    print(type(args))
+
+multiple_items("dave", "john", "sara")
+
+
+def mult_named_items(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+mult_named_items(first = "Dave", last = "Gray")
+
+value = "y"
+count = 0
+
+while value:
+    count += 1
+    print(count)
+    if (count == 5):
+        break
+    else:
+        value = 0 
+        continue
