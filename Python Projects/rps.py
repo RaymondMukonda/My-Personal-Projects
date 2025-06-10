29b630129acc939a4d9382a52a8a3cfeb4330985
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-10 10:50:24 +0200

    rps.py

--- Python Projects/rps.py
+++ Python Projects/rps.py
@@ -2,53 +2,72 @@ import sys
 import random
 from enum import Enum
 
-game_count = 0
+def rps():
+    game_count = 0
+    player_wins = 0
+    python_wins = 0
 
-def play_rps():
-    class RPS(Enum):
-        ROCK = 1
-        PAPER = 2
-        SCISSORS = 3
+    def play_rps():
+        nonlocal player_wins
+        nonlocal python_wins
 
-    while True:
-        print("")
-        playerchoice = input("Enter...\n1 for Rock, \n\n2 for Paper, or \n\n3 for Scissors:\n\n")
-        player = int(playerchoice)
 
-        if player < 1 or player > 3:
-            sys.exit("You must enter 1, 2, 3.")
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSORS = 3
 
-        computerchoice = random.choice("123")
-        computer = int(computerchoice)
+        while True:
+            print("")
+            playerchoice = input("Enter...\n1 for Rock, \n\n2 for Paper, or \n\n3 for Scissors:\n\n")
+            player = int(playerchoice)
 
-        print("")
-        print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
-        print("Pyton chose " + str(RPS(computer)).replace('RPS.', '') + ".")
-        print("")
-        def decide_winner(player, computer):
-            if player == 1 and computer == 3:
-                return "You have won!😲🔥"
-            elif player == 2 and computer == 1:
-                return "You have won!😊👌" 
-            elif player == 3 and computer == 2:
-                return "You have won!😆✨" 
-            elif player == computer:
-                return "It's a tie!😒"
-            else:
-                return "Python has won🐍🐍"
+            if player < 1 or player > 3:
+                sys.exit("You must enter 1, 2, 3.")
 
-        game_result = decide_winner(player, computer)
-        print(game_result)
+            computerchoice = random.choice("123")
+            computer = int(computerchoice)
 
-        global game_count
-        game_count += 1
-        print("\nGame Count: " + str(game_count))
+            print("")
+            print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+            print("Pyton chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+            print("")
+            def decide_winner(player, computer):
+                nonlocal player_wins
+                nonlocal python_wins
+                if player == 1 and computer == 3:
+                    player_wins += 1
+                    return "You have won!😲🔥"
+                elif player == 2 and computer == 1:
+                    player_wins += 1
+                    return "You have won!😊👌" 
+                elif player == 3 and computer == 2:
+                    player_wins += 1
+                    return "You have won!😆✨" 
+                elif player == computer:
+                    return "It's a tie!😒"
+                else:
+                    python_wins += 1
+                    return "Python has won🐍🐍"
 
-        play_again = input("\nWould you like to play again? (y/n): ").lower()
-        if play_again != "y":
-            print("Thanks for playing!")
-            break
+            game_result = decide_winner(player, computer)
+            print(game_result)
 
-# sys.exit("Bye! 👋")
+            nonlocal game_count
+            game_count += 1
+            print("\nGame Count: " + str(game_count))
+            print("\nPlayer wins: " + str(player_wins))
+            print("\nPyton wins: " + str(python_wins))
 
-play_rps()
+            play_again = input("\nWould you like to play again? (y/n): ").lower()
+            if play_again != "y":
+                print("Thanks for playing!")
+                break
+
+    return play_rps
+
+    # sys.exit("Bye! 👋")
+
+play = rps()
+
+play()
---
+++ practise.py
@@ -0,0 +1,78 @@
+# def sum(num1,num2):
+#     return num1 + num2
+
+# total = sum(2, 3)
+# print(total)
+
+# def multiple_items(*args):
+#     print(args)
+#     print(type(args))
+
+# multiple_items("dave", "john", "sara")
+
+
+# def mult_named_items(**kwargs):
+#     print(kwargs)
+#     print(type(kwargs))
+
+# mult_named_items(first = "Dave", last = "Gray")
+
+# value = "y"
+# count = 0
+
+# while value:
+#     count += 1
+#     print(count)
+#     if (count == 5):
+#         break
+#     else:
+#         value = 0 
+#         continue
+
+# name = "dave"
+# count = 2
+
+# def greeting():
+#     color = "blue"
+#     global count 
+#     count += 1
+#     print(count)
+
+#     def another(name):
+#         nonlocal color
+#         color = "red"
+#         print(color)
+#         print(name)
+
+#     another("rammy")
+
+
+# greeting()
+
+
+def parent_function(person, coins):
+    # coins = 3
+
+    def play_game():
+        nonlocal coins
+        coins -= 1
+
+        if coins > 1:
+            print(f"\n{person} has {coins} coins left")
+        elif coins == 1:
+            print("\n" + person + " has " + str(coins) + " coin left.")
+        else:
+            print("\n" + person + " Is out of coins.")
+            
+
+    return play_game
+
+Rammy = parent_function("Rammy", 3)
+susan = parent_function("susan", 5)
+
+Rammy()
+Rammy()
+susan()
+Rammy()
+
+
