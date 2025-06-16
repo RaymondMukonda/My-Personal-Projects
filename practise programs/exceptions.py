3b3ac269cbf49480d8bbfb65e967d97d9fb28023
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-16 12:35:51 +0200

    excetions.py

--- practise programs/exceptions.py
+++ practise programs/exceptions.py
@@ -1,4 +1,17 @@
+
+x = 2
 try:
-    print(x)
+    # print(x/1)
+    if not type(x) is str:
+        raise TypeError("only strings  are allowed")
 except NameError:
     print("NameErros means something is undefined.")
+except ZeroDivisionError:
+    print("Please do not divide by zero.")
+except Exception as error:
+    print(error)
+else:
+    print("No errors!")
+
+finally:
+    print("I'm going to print with or without an errors.")
