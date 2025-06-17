059660cf994cb9d88ae99d111fee914406769f5f
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-17 22:35:41 +0200

    files.py

--- practise programs/files.py
+++ practise programs/files.py
@@ -1,2 +1,38 @@
 f = open("names.txt")
+# print(f.read())
+# print("")
+# print(f.readline())
+# print(f.readline())
+for line  in f:
+    print(line)
+
+f.close()
+
+try:
+    f = open("name_list.txt")
+    print(f.read())
+except:
+    print("The file you are trying to open doesnt not exist.")
+finally:
+    f.close()
+
+# append - creates a file if it doesnts exist
+
+f = open("names.txt", "a")
+f.write("\nNdeka")
+f.close()
+
+f = open("names.txt")
 print(f.read())
+f.close()
+
+###write (overwrite) creates a new file
+f = open("context.txt", "w")
+f.write("I deleted all context")
+f.close()
+
+
+f = open("context.txt")
+print(f.read())
+f.close()
+
