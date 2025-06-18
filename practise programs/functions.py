15f82680825b27772507100d5a233b29fbdcabb9
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-18 14:26:18 +0200

    functions.py

---
+++ practise programs/functions.py
@@ -0,0 +1,13 @@
+# def greet():
+#     print("hello there")
+#     print("welcome abroad")
+
+
+## its args and be placed where you see fit
+# def greet(name, age):
+#     print(f"hello {name} im awere that you are {age} of age right?")
+
+# greet("raymond", 21)
+
+#args allows a function to acccept any number of positional arguments, these arguments are accedd as tuples inside the 
+
