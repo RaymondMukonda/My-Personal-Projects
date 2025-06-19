97eaae178ad8867fff943231c09429bc9aee7c9c
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-19 18:05:15 +0200

    class.py

--- practise programs/class.py
+++ practise programs/class.py
@@ -1,11 +1,12 @@
 class Dog:
 
     def bark(self):
-        print("bark")
+        print("the dog is barking")
 
     def add_one(self, x):
         return x + 1
 
 
 d = Dog()
+d.bark()
 print(d.add_one(5))
