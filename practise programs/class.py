d69b89e7af2008c86c38b394b37cb42299d56d53
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-19 18:26:42 +0200

    class.py

--- practise programs/class.py
+++ practise programs/class.py
@@ -1,12 +1,30 @@
+# class Dog:
+
+#     def bark(self):
+#         print("the dog is barking")
+
+#     def add_one(self, x):
+#         return x + 1
+
+# d = Dog()
+# d.bark()
+# print(d.add_one(5))
+
+###innit allows you to name the dog funtions 
+
 class Dog:
 
-    def bark(self):
-        print("the dog is barking")
+    def __init__(self, name):
+        self.name = name
+        print(name)
 
-    def add_one(self, x):
-        return x + 1
-
-
-d = Dog()
-d.bark()
-print(d.add_one(5))
+    #anoher way to print what vaule is givven 02
+    def get_name(self):
+        return self.name
+# this is calling the the fuction to print on its own without print
+d = Dog("tom")
+d2 = Dog("molly")
+print("")
+#this will get what ever name is stored within the assgined d or d2 arguments
+print(d.get_name())
+print(d2.get_name()) #after printing the output should be the same.
