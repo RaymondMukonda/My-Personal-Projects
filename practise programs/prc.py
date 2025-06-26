8500364195418f9d9a12dde6549cfa9824194163
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-26 19:41:51 +0200

    prct.py

---
+++ practise programs/prc.py
@@ -0,0 +1,12 @@
+def greet():
+    # name = input("what is your name? ")
+    # age = input("how old are you? ")
+    # print(f"good evening {name} and awre that you are {age} of age right?")
+    num1 = int(input("give me two numers to add: "))
+    num2 = int(input("second numner: "))
+    # answer1 = int(num1)
+    # answer2 = int(num2)
+    total = num1 + num2
+
+    print(f"you total is {total}")
+greet()
