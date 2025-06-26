92e45cf3dcedd10737d6fd164596b9772186b433
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-26 20:12:01 +0200

    prc.py

--- practise programs/prc.py
+++ practise programs/prc.py
@@ -4,8 +4,8 @@ def greet():
     # print(f"good evening {name} and awre that you are {age} of age right?")
     num1 = int(input("give me two numers to add: "))
     num2 = int(input("second numner: "))
-    # answer1 = int(num1)
-    # answer2 = int(num2)
+    answer1 = int(num1)
+    answer2 = int(num2)
     total = num1 + num2
 
     print(f"you total is {total}")
