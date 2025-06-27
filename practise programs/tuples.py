d5ca1208df71e3f87739a3d31b55bef4bb9a3abb
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-27 19:16:03 +0200

    tuples.py

---
+++ practise programs/tuples.py
@@ -0,0 +1,9 @@
+#Tuple: oredered, immutable, allows duplicate elements
+#tuples are created with parenthasis and separeted by a comma
+mytuple = {"max", 26, "south africa"}
+print(mytuple)
+print("")
+# you can still create tuples without parenthesis
+mytuple = "max", 26, "south africa"
+print(type(mytuple))
+print(mytuple)
