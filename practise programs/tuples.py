671dbaa67e1e281e147731c353addaa13f397f85
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-27 19:26:39 +0200

    tuples.py

--- practise programs/tuples.py
+++ practise programs/tuples.py
@@ -7,3 +7,16 @@ print("")
 mytuple = "max", 26, "south africa"
 print(type(mytuple))
 print(mytuple)
+#to see or get index of something 
+print(mytuple.index("max"))
+print("")
+#you can convert a tuple into a list
+my_tuple = "a", "b", "c", "d", "e"
+my_tuple = list(my_tuple) #by adding the word list you turn this diction into a list
+print(my_tuple)
+print(type(my_tuple))
+#to turn it back into a tuple use
+my_tuple2 = tuple(my_tuple)
+print(type(my_tuple2))
+print(my_tuple2)
+print("")
