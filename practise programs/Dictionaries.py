5293b49391821ff0b36234983b15c6420a68a232
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-28 17:16:09 +0200

    Dictionaries.py

--- practise programs/Dictionaries.py
+++ practise programs/Dictionaries.py
@@ -17,6 +17,22 @@ print(value)
 
 #ths will help you check in the name max is sortered within. dont forget
 # name is a (key) and max is a (value) so to check youd need to call it like below
-if "max" in mydict.values():
+if "max" in mydict.values(): # this soecifically only gets max but you can get all values by doing this 
     print("name max is sorted within")
 print("")
+#this print out all the values
+for value in mydict.values(): #you can place the word key where value is to get the values.
+    print(value)
+print("")
+# or you can print both the key and values at thre same time
+for key, value in mydict.items():
+    print(key, value)
+print("")
+#OR YOU CAN do it this way
+if "name" in mydict:
+    print(mydict["name"]) #it should print out max
+#  incase someone places a wrong key you can promt them to do this 
+try:
+    print(mydict["lastname"])
+except:
+    print("That value does not exist in my dict")
