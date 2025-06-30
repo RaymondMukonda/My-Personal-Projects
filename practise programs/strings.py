b28c32d6341696b8b57e92d76fadbf6b60092c03
RaymondMukonda <Raymondkogen@gmail.com>
2025-06-30 18:28:13 +0200

    strings.py

---
+++ practise programs/strings.py
@@ -0,0 +1,17 @@
+# strings: ordered, immutable, text representation
+my_string = "I'm a programmer "
+# go over mutiple lines / it will be printed downing down wards
+my_string = """Hello
+
+World"""
+print(my_string)
+# but if you put a slash like this it will then not print in a new line 
+my_string1 = """Hello \
+
+World"""
+print(my_string1)# this will print out one below the other without the indented space that was given in the ecode
+# this is how you can get / fetch fro strings 
+print("")
+my_string2 = "Hello World"
+char = my_string2[1] #you can also get negative numbers -1
+print(char)
