80441a8818ce022a4d1373ebbdff525207783aec
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-02 10:58:11 +0200

    collections.py

--- practise programs/collections_prct.py
+++ practise programs/collections_prct.py
@@ -14,8 +14,30 @@ pt = Point(1, -4)
 print(pt) # this will print everything together you can fect the two var individually
 print(pt.x, pt.y) #see diffrence in output
 print(type(pt))
-
+print("")
 
 from collections import OrderedDict
 # this is the same as an ordinerey dict only diffrenece is this one rembers 
 # the order the item was inserted. but ts now nuneccry bec new pthon dict can do them same 
+
+ordered_dict = OrderedDict()
+ordered_dict['a'] = 1
+ordered_dict['b'] = 2
+ordered_dict['c'] = 3
+ordered_dict['e'] = 4
+ordered_dict['f'] = 5
+print(ordered_dict)
+print("")
+
+from collections import defaultdict
+d = defaultdict(int) #you can place a float, list etc
+d["a"] = 1
+d["b"] = 2
+print(d["a"])
+print(d["c"]) #this will bring back the defualt interger we placed at the top if the key we looking for doesnt exist.
+print("")
+
+from collections import deque
+# its a double ended que that removes characters from both ends / front and
+# 3 back
+
