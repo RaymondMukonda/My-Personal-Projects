abc479c711598677a438abcdebcac6defe452d1e
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-02 11:28:24 +0200

    itertool_s.py

--- practise programs/itertool_s.py
+++ practise programs/itertool_s.py
@@ -10,3 +10,10 @@ c = [1, 2]
 d = [3]
 prod2 = product(c,d, repeat=2)
 print(list(prod2)) # see out put in terminal
+print("")
+
+from itertools import permutations
+# with this you get to see every outcome of that code that can be written
+f = [1, 2, 3]
+perm = permutations(f)
+print(list(perm))
