1695306046dd70be8949d3691af06b13be33fc71
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-07 14:26:52 +0200

    json

--- practise programs/json_tut.py
+++ practise programs/json_tut.py
@@ -3,5 +3,5 @@ import json
 person = {"name": "John", "age": 25, "city": "New York", "haschildren": False, "title": ["engineer", "programmer"] }
 
 # to turn tis into json formate
-personJSON = json.dumps(person)
+personJSON = json.dumps(person, indent=4)
 print(personJSON)
