403741b4f3eac180fb037426d96d85bf4009ab08
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-07 14:41:33 +0200

    class

---
+++ practise programs/random_.py
@@ -0,0 +1,7 @@
+#the numbers seem random but are reproducable
+import random
+
+#this will print a roandom number from 0-1
+a = random.random()
+print(a) # evrtime you print it will give you different number
+
