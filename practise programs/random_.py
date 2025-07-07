5835f728e36725f77e9df1812e5a78f938211370
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-07 14:51:00 +0200

    rand

--- practise programs/random_.py
+++ practise programs/random_.py
@@ -2,8 +2,17 @@
 import random
 
 #this will print a roandom number from 0-1
- # evrtime you print it will give you different number
+a = random.random()
+print(a) # evrtime you print it will give you different number
 
-# you can give it a specific range to choose  from
+# you can give it a specific range to choose  from floats
 b = random.uniform(1, 11)
 print(b)
+# if you want a fixed nummber you can 
+c = random.randint(1, 11)
+print(c)
+
+# this will never pick the upper number(last which is 10)
+d = random.randrange(1, 10)
+print(d)
+print("")
