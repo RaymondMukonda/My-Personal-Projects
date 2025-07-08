c473dce91e7b7b02fea017bab5df093251df4b1e
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-08 15:39:54 +0200

    dec

--- practise programs/decorators_.py
+++ practise programs/decorators_.py
@@ -75,3 +75,25 @@ def start_end_decorator(func):
         print("end") # then it will do something after the fuction
         return result
     return wrapper
+
+#######################################################################
+#  decorator can take a function witin it also 
+print("")
+import functools
+
+def repeat(num_times):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(num_times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator_repeat
+
+
+@repeat(num_times=3)
+def greet(name):
+    print(f"Hello {name}")
+
+greet("Rammy")
