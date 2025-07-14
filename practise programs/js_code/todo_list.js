a90033ed9f416e40f6714574fe257d1f07a8d09c
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-14 13:40:48 +0200

    arry

--- practise programs/js_code/todo_list.js
+++ practise programs/js_code/todo_list.js
@@ -1,4 +1,15 @@
-const todolist = [];
+const todolist = ['Make dineer', 'wash dishes'];
+
+let todolistHTML = '';
+
+for (let i = 0; i < todolist.length; i++) {
+    const todo = todolist[i];
+    const html = `<p>${todo}<p>`;
+    todolistHTML += html;
+}; 
+
+
+
 
 function addTodo() {
     const inputElemnt  = document.querySelector('.js-name-input');
