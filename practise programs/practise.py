fdf5970346fa99024767df0ca6c1b07ae7d54408
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-16 00:21:51 +0200

    practise

--- practise programs/practise.py
+++ practise programs/practise.py
@@ -1,13 +1,4 @@
-def addnum(num1, num2):
-    # add_num = num1 + num2
-    return num1 + num2
-    # print(add_num)
-
-
-sum1 = int(input("Give a number to add: "))
-sum2 = int(input("Give another number to add: "))
-
-answer = addnum(sum1, sum2)
-print("The answer is:",answer)
+for i in range(11):
+    print(i)
 
 
