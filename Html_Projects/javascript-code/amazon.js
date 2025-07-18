790c8104b554faca7a703ce9e20f0ee29dbb5ded
RaymondMukonda <Raymondkogen@gmail.com>
2025-07-19 01:50:55 +0200

    ama

--- Html_Projects/javascript-code/amazon.js
+++ Html_Projects/javascript-code/amazon.js
@@ -1,36 +1,4 @@
-const products = [{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}, {
-  image: 'images/products/black-2-slot-toaster.jpg',
-  name: '2 Slot Toaster - Black',
-  rating: {
-    stars: 5,
-    count: 2197
-  },
-  priceCents: 1899
-}];
+
 
 let productsHTML = '';
 
@@ -80,7 +48,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart">
             Add to Cart
           </button>
         </div>
@@ -91,3 +59,10 @@ console.log(productsHTML);
 
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+ .forEach((button) => {
+  button.addEventListener('click', () => {
+
+  });
+ });
