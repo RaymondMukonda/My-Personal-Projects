b4528693779c37be0e6c28f816c96e09a7cc254d
Raymond Mukonda <RaymondKogen@gmail.com>
2026-01-07 08:18:15 +0200

    tracking key words

--- Html_Projects/javascript-code/Dom-rsp-project.js
+++ Html_Projects/javascript-code/Dom-rsp-project.js
@@ -39,6 +39,17 @@ document.querySelector('.js-paper-button').addEventListener('click', () => {
     playGame('paper');
 }) 
 
+// This code keeps track of every button you press on your keyboard
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    }
+})
+
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
 
