4632c31160536ef0f5603e705af2d17d49762e2a
Raymond Mukonda <RaymondKogen@gmail.com>
2026-01-08 08:06:06 +0200

    responsive features cart

--- Html_Projects/javascript-code/amazon.js
+++ Html_Projects/javascript-code/amazon.js
@@ -89,6 +89,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       cartQuantity += item.quantity;
     });
 
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     console.log(cartQuantity);
     console.log(cart);
   });
