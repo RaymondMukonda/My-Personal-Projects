5d03b36d596f117dbb8a9014a0c795d5ecd9b7bb
Raymond Mukonda <RaymondKogen@gmail.com>
2026-01-15 16:34:17 +0200

    more erros fixed

--- Html_Projects/amazon-project/scripts/checkout.js
+++ Html_Projects/amazon-project/scripts/checkout.js
@@ -42,12 +42,31 @@ cart.forEach((cartItem) => {
         }
     });
 
-    
+
+    const deliveryOptionId = cartItem.deliveryOptionId;
+
+
+    let deliveryOption;
+
+
+    deliveryOptions.forEach((option) => {
+        if (option.id === deliveryOptionId) {
+            deliveryOption = option;
+        }
+    });
+
+    const today = dayjs();
+    const deliveryDate = today.add(
+        deliveryOption.deliveryDays, 'days'
+    );
+    const dateString = deliveryDate.format(
+        'dddd, ,MMMM D'
+    );
 
     cartSummaryHTML += ` 
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
-            Delivery date: Tuesday, June 21
+            Delivery date: ${dateString}
         </div>
 
         <div class="cart-item-details-grid">
@@ -84,6 +103,9 @@ cart.forEach((cartItem) => {
     </div>
     `;
 });
+
+
+
 
 function deliveryOptionsHTML(matchingProduct, cartItem) {
 
@@ -126,8 +148,13 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     return html;
 }
 
+
+
+
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 // This div contains all my check out html 
+
+
 
 document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click', () => {
