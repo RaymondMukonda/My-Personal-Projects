22a3cd79a5e4e6cb0ae59b7fcd88a0b803196475
Raymond Mukonda <RaymondKogen@gmail.com>
2026-01-15 16:18:04 +0200

    delivery id

--- Html_Projects/amazon-project/scripts/utils/money.js
+++ Html_Projects/amazon-project/scripts/utils/money.js
@@ -1,3 +1,7 @@
 export function formatCurrency(priceCents) {
     return (priceCents / 100).toFixed(2);
 }
+
+// Each file can only have 1 default export, us only if you expoting one thing from the line to have clean code
+// example ( import formatCurrency from './utils/money.js'; ) in your checkout file 
+export default formatCurrency;
