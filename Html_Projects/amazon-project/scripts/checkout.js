dff24a3064842772c1cb23626c0a7b7a631ca48c
Raymond Mukonda <RaymondKogen@gmail.com>
2026-01-15 12:43:38 +0200

    problem solved

--- Html_Projects/amazon-project/scripts/checkout.js
+++ Html_Projects/amazon-project/scripts/checkout.js
@@ -1,12 +1,14 @@
 import {
     cart
 } from '../data/cart.js';
+
 import {
     products
 } from '../data/products.js';
+
 import {
     formatCurrency
-} from './money.js';
+} from '../scripts/utils/money.js';
 
 let cartSummaryHTML = '';
 
