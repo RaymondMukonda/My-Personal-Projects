81b700cbf6a8cd1b2acd98e98fed84a4ee62d0e8
Raymond Mukonda <RaymondKogen@gmail.com>
2026-01-15 12:55:00 +0200

    fixed radio product

--- Html_Projects/amazon-project/scripts/amazon.js
+++ Html_Projects/amazon-project/scripts/amazon.js
@@ -6,6 +6,10 @@ import {
   products
 } from '../data/products.js';
 
+import {
+  formatCurrency
+} from '../scripts/utils/money.js';
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -29,7 +33,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
