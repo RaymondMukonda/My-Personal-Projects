ea282eb6ebcf99b827b7a41205c50d20f469c2bb
Raymond Mukonda <Raymondkogen@gmail.com>
2026-03-12 21:42:41 +0200

    p

--- practise programs/scripts.js
+++ practise programs/scripts.js
@@ -1,3 +1,15 @@
-a = 5; b = 7; c = a + b;
+let userRole = "editor";
 
-console.log(c);
+switch (userRole) {
+  case "admin":
+    console.log("Access granted: Full control");
+    break;
+  case "editor":
+    console.log("Access granted: Can edit content");
+    break;
+  case "viewer":
+    console.log("Access granted: Read-only mode");
+    break;
+  default:
+    console.log("Access denied: Unknown role");
+}
