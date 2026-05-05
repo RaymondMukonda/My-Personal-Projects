4c0a0c0222c196ab7f0c08d776d3a4417ef01b9b
Raymond Mukonda <Raymondkogen@gmail.com>
2026-05-05 19:16:46 +0200

    students

---
+++ API/scripts.mjs
@@ -0,0 +1,22 @@
+async function getData() {
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/RaymondMukonda/My-Personal-Projects/main/API/data.json");
+    const students = await response.json();
+    console.log(students);
+  } catch (error) {
+    console.error("There was an error:", error);
+  }
+}
+
+getData();
+
+
+function greetUser(student) {
+  console.log(`Hello ${student.name} from ${student.school}, year ${student.year}`);
+}
+
+getData();
+
+
+
+
