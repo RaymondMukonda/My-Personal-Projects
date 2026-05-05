7fa69d6147ac78337034ec5496e8bb7083f4dc63
Raymond Mukonda <Raymondkogen@gmail.com>
2026-05-05 18:35:31 +0200

    git hub api

--- API/scripts.js
+++ API/scripts.js
@@ -1,64 +1,25 @@
-function Person(name) {
-  this.name = name;
-  return `Hello I'm ${this.name}`;
+function greetUser(user) {
+    console.log(`hello user from feteched data ${user.name}`)
 }
 
-const me = new Person('rammy');
-console.log(me);
-console.log(me.name);
+async function getData() {
+    try {
+
+        const response = await fetch('data.json');
+        const userData = await response.json();
+
+        greetUser(userData);
+
+        console.log('Data fetched', userData);
+        console.log('name: ', userData.name);
+        console.log('school: ', userData.school);
+        console.log('year: ', userData.year);
 
 
+    } catch(error) {
+        console.log('the was an error: ', error)
+    }
+}
 
+getData();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log();
