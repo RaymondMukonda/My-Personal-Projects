3d20b8b802fdb448c85b51135955c26e9a34a8da
Raymond Mukonda <Raymondkogen@gmail.com>
2026-05-08 10:43:41 +0200

    practise api

--- API/scripts.mjs
+++ API/scripts.mjs
@@ -3,6 +3,10 @@ async function getData() {
     const response = await fetch("https://raw.githubusercontent.com/RaymondMukonda/My-Personal-Projects/main/API/data.json");
     const students = await response.json();
     console.log(students);
+
+    const me = greetUser(students[0]);
+    const you = greetUser(students[1]);
+
   } catch (error) {
     console.error("There was an error:", error);
   }
@@ -15,7 +19,6 @@ function greetUser(student) {
   console.log(`Hello ${student.name} from ${student.school}, year ${student.year}`);
 }
 
-getData();
 
 
 
