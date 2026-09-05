4fc1069995623b799c1442f956c09320094686b8
Raymond Mukonda <Raymondkogen@gmail.com>
2026-09-05 14:38:41 +0200

    react new concepts and jsx

--- Big3/CSharp/MyApp/Program.cs
+++ Big3/CSharp/MyApp/Program.cs
@@ -14,7 +14,7 @@ namespace MyMain
         {
             public string? name { get; set; }
             public string? surname { get; set; }
-            public int age { get; set; }
+            public int? age { get; set; }
         }
 
 
